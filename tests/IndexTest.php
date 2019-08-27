7f45b8bc80dc491eeb7f586ce11c99ad474d5059
Martin Price <martin.price@emailcenteruk.com>
2019-08-27 11:16:00 +0100

    Add tests for index

---
+++ tests/IndexTest.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Phlib\SchemaChange\Tests;
+
+use Phlib\SchemaChange\Formatter;
+use Phlib\SchemaChange\Formatter\TestFake as FakeFormatter;
+use Phlib\SchemaChange\Index;
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    /**
+     * @var Formatter
+     */
+    private $formatter;
+
+    public function setUp(): void
+    {
+        $this->formatter = new FakeFormatter();
+        parent::setUp();
+    }
+
+    public function testIndexWithGeneratedName()
+    {
+        $index = new Index($this->formatter, 'table_name', 'column_1');
+        static::assertEquals('KEY `table_name_column_1_idx` (`column_1`)', (string)$index);
+    }
+
+    public function testComplexIndexWithGeneratedName()
+    {
+        $index = new Index($this->formatter, 'table_name', 'column_1', 'column_2');
+        static::assertEquals('KEY `table_name_column_1_column_2_idx` (`column_1`, `column_2`)', (string)$index);
+    }
+
+    public function testNamedIndex()
+    {
+        $index = (new Index($this->formatter, 'table_name', 'column_1'))
+            ->name('my_idx');
+        static::assertEquals('KEY `my_idx` (`column_1`)', (string)$index);
+    }
+
+    public function testUniqueIndex()
+    {
+        $index = (new Index($this->formatter, 'table_name', 'column_1'))
+            ->name('my_idx')
+            ->unique();
+        static::assertEquals('UNIQUE KEY `my_idx` (`column_1`)', (string)$index);
+    }
+}
